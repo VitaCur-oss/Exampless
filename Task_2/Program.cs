55ba97df4033f105ea2aa5ecf890f7c24f07a76d
v <antyan2010@rambler.ru>
2022-12-16 10:56:02 +0300

    Квадрат числа или нет

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+//a = 25; b = 5 -> да
+//a = 2 b = 10 -> нет
+//a = 9; b = -3 -> да
+//a = -3 b = 9 -> нет
+
+using static System.Console;
+Clear();
+
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+if ((b * b) == a) {WriteLine("Yes");}
+else {WriteLine("No");}
