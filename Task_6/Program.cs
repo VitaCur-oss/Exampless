dce716ecbe8837f577619d48747a955fea3f4a8f
v <antyan2010@rambler.ru>
2022-12-17 11:05:14 +0300

    Added Task about Dog and Friends

---
+++ Task_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+Clear();
+
+int First_Friend_Speed = 1;
+int Second_Friend_Speed = 2;
+int Dog_Speed = 5;
+int Distance = 10000;
+int Distance_to_meet = 10;
+int turn = 2;
+int count = 0;
+int time = 0;
+
+
+while (Distance_to_meet < Distance)
+{
+    turn = 2;
+    time = Distance / (Dog_Speed + Second_Friend_Speed);
+    Distance = Distance - (First_Friend_Speed + Second_Friend_Speed) * time;
+    WriteLine($"Расстояние между друзьями = {Distance}.");
+    count ++;
+    turn = 1; 
+}
+WriteLine($"Собака пробежала: {count} раз между друзьями!");
