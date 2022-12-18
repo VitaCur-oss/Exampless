4cc86a81b7acd25cd50859b15e97c1af675642e9
v <antyan2010@rambler.ru>
2022-12-18 16:18:51 +0300

    Find number with function

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+// int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+// // WriteLine(array[4]);
+// array[1] = 45;
+// WriteLine(array[1]);
+
+// int[] array = { 1, 98, 38, 5, 52, 7, 98, 54, 45 };
+// // WriteLine(array.Length);
+// int n = array.Length;
+// int find = 98;
+// int index = 0;
+// while(index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         WriteLine(index);
+//         break;
+//     }
+//     index++;
+// }
+int[] array = new int[10];
+
+
+// WriteLine(array);
+void FillArray(int[] Collection)
+{
+    int lenght = Collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        Collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] Col)
+
+
+{
+    int count = Col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        WriteLine(Col[position]);
+        position++;
+    }
+}
+
+FillArray(array);
+array[2] = 4;
+array[3] = 4;
+PrintArray(array);
+
+int IndexOff(int[] collection, int find)
+{
+    int index = 0;
+    int position = -1;
+    int lenght = collection.Length;
+    while (index < lenght)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int pos = IndexOff(array, 44);
+WriteLine(pos);
