563426d4203751acae3fe21eea92965ca1097e84
v <antyan2010@rambler.ru>
2022-12-21 12:37:15 +0300

    Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.

--- Task_11/Program.cs
+++ Task_11/Program.cs
@@ -1,2 +1,7 @@
 ﻿using static System.Console;
 Clear();
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+int a = int.Parse(ReadLine()!);
+int b = int.Parse(ReadLine()!);
+
+WriteLine(a * a == b || b * b == a ? "Yes" : "No");
