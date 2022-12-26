6b04ab3ead3c0c996e031e9aebcf71dc3639ca43
v <antyan2010@rambler.ru>
2022-12-26 15:56:11 +0300

    Программа, которая на вход принимает массив из случайных чисел, а на выходе показывает сумму всех двухзначных чисел!

---
+++ Task_14/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+Clear();
+// Программа, которая на вход принимает массив из случайных чисел,
+//  а на выходе показывает сумму всех двухзначных чисел!
+
+Write("Введите длинну создаваемого списка: ");
+int len = int.Parse(ReadLine()!);
+int[] array = new int[len];
+int sum = 0;
+int index = 0;
+while (index < len)
+{
+    array[index] = Random.Shared.Next(99);
+    Write(array[index] + " ");
+    
+    if (array[index] > 9)
+    {
+        sum = sum + array[index];
+    }
+    index++;
+}
+// WriteLine(array);
+WriteLine("----");
+WriteLine(sum);
