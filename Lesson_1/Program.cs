f90bb286a881d8c13950783d457dffc096fdb5fc
v <antyan2010@rambler.ru>
2023-01-09 13:13:25 +0300

    Hello World!

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+Clear();
+
+namespace HelloWorld
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+            WriteLine("Hello world!");
+        }
+    }
+}
