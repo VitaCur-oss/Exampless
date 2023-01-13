621e143e9e7cfc146cb1fa4ae56023db756ac14b
v <antyan2010@rambler.ru>
2023-01-13 09:16:34 +0300

    Программа, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А

---
+++ Task_17/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+Clear();
+
+// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Write("Выедите число: ");
+int number = int.Parse(ReadLine()!);
+
+int sum = GetSum(number);
+WriteLine($"Sum number 1...A = {sum}");
+
+
+int GetSum(int A)
+{
+    int result = 0;
+    while (A > 0)
+    {
+        result += A;
+        A--;
+    }
+    return result;
+}
+
