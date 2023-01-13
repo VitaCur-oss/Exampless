4dc52d20a4e1b9e406a8b33caa0c8ec1174f9883
v <antyan2010@rambler.ru>
2023-01-13 12:28:30 +0300

    Программа, которая суммирует числа от 1 до N, если число делимое от N делиться без остатка.

---
+++ Task_20/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+Clear();
+
+// Программа, которая суммирует числа от 1 до N, если число делимое от N делиться без остатка.
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int sumi = GetSumI(number);
+WriteLine(sumi);
+
+
+int GetSumI(int A)
+{
+    int c = A;
+    int result = 0;
+    while (c > 0)
+    {
+        if (A % c == 0)
+        {
+            result += c;
+        }
+        c--;
+    }
+    return result;
+}
