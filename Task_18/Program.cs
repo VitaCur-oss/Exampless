614f6bff22347c5386a115a2eaff0a7a01e9a216
v <antyan2010@rambler.ru>
2023-01-13 11:12:17 +0300

    Программа, которая принимает на вход число и выдаёт количество цифр в числе.

---
+++ Task_18/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int num = GetNum(number);
+WriteLine(num);
+
+int GetNum(int A)
+{
+    int count = 0;
+    while (A > 0)
+    {
+        A = A / 10;
+        count++;
+    }
+    return count;
+}
+        
