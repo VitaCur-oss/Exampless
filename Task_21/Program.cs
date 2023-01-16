caef19e10b1efdb7d65884a281635f440ad39ece
v <antyan2010@rambler.ru>
2023-01-16 10:15:47 +0300

    Программа, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.

---
+++ Task_21/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+Clear();
+
+// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+Write("Введите длинну массива: ");
+int arr = int.Parse(ReadLine()!);
+
+GetBinarryArr(arr);
+PrintArray(GetBinarryArr(arr));
+
+
+int[] GetBinarryArr(int ar)
+{
+    int[] res = new int[ar];
+    for (int j = 0; j < ar; j++)
+    {
+        res[j] = new Random().Next(0, 2);
+        // Write(res[j]);
+    }
+    return res;
+}
+
+void PrintArray(int[] arrr)
+{
+    for (int j = 0; j < arr; j++)
+    {
+        Write(arrr[j]);
+    }
+} 
