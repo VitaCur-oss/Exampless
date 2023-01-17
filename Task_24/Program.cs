13cdd3a83afe8551c2d6ceb306e851c88dc84c15
v <antyan2010@rambler.ru>
2023-01-17 15:08:04 +0300

    Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.

---
+++ Task_24/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+Clear();
+
+// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] array = { 1, 5, 8, 6, 1, -4, -9, -2 };
+bool find = Find(array, 6);
+PrintArray(array);
+WriteLine($" -> {find}");
+
+bool Find(int[] farray, int num)
+{
+    for (int i = 0; i < farray.Length; i++)
+    {
+        if (farray[i] == num) return true;
+    }
+    return false;
+}
+
+void PrintArray(int[] arr)
+{
+    Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Write($"{arr[i]}, ");
+    }
+    Write($"{arr[arr.Length - 1]}]");
+}
