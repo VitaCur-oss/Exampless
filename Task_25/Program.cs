c4b41366fbfc55abc521aa6e7234a1a01eb773e2
v <antyan2010@rambler.ru>
2023-01-18 09:50:03 +0300

    Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].

---
+++ Task_25/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+Clear();
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+//  Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+int[] Arr = GetArray(123, 1, 110);
+PrintArray(Arr);
+WriteLine();
+int counter = Count(Arr);
+WriteLine(counter);
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+    }
+    return result;
+}
+
+int Count(int[] arr)
+{
+    int cou = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] >= 10 && arr[i]<= 99) cou++;
+    }
+    return cou;
+}
+
+void PrintArray(int[] array)
+{
+    Write("[");
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        Write($"{array[i]}, ");
+    }
+    Write($"{array[array.Length -1]}]");
+}
