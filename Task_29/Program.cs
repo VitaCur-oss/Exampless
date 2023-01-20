6b4ca95f4fd157e50785f99810ca7414c3479cd2
v <antyan2010@rambler.ru>
2023-01-20 15:18:34 +0300

    Программа, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.

---
+++ Task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+//  может ли существовать треугольник с сторонами такой длины.
+
+
+
+using static System.Console;
+Clear();
+
+Write("Введите стороны треугольника через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+WriteLine();
+bool triangle = IsTriangle(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]));
+WriteLine(triangle);
+
+bool IsTriangle(int A, int B, int C)
+{
+    if (A + B > C && A + C > B && B + C > A) return true;
+    return false;
+}
