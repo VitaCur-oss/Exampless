36135170d22a9957bc06c9186ad340fa449c09bb
v <antyan2010@rambler.ru>
2023-01-20 15:19:37 +0300

    Программа, которая будет преобразовывать десятичное число в двоичное.

---
+++ Task_30/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+Clear();
+
+// **Задача 42:** Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Write("Введите число, которое переведем из десятичной системы в двоичную: ");
+int num = int.Parse(ReadLine()!);
+WriteLine($"{num} -> {DecInBin(num)}");
+
+
+string DecInBin(int number)
+{
+    if (number == 0) return "0";
+    string result = String.Empty;
+    int rum;
+    while(number > 0)
+    {
+        rum = number % 2;
+        number /= 2;
+        result = rum.ToString() + result;
+    }
+    return result;
+}
