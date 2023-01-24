67ada5ca14a2b953b3a222e3cc5cd7c5f7c55c10
v <antyan2010@rambler.ru>
2023-01-24 12:53:55 +0300

    Счастливые билетики

---
+++ Task_31/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+Clear();
+
+// Счастливые билетики
+
+Write("Введите номер билетика(шесть цифр): ");
+string[] ticket = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+bool happy = IsHappy(ticket);
+WriteLine(happy);
+
+bool IsHappy(string[] arr)
+{
+    int first = 0;
+    int second = 0;
+
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        first += int.Parse(arr[i]);
+        
+        second += int.Parse(arr[arr.Length -1 - i]);
+    }
+    WriteLine(first);
+    WriteLine(second);
+    return (first == second);
+}
