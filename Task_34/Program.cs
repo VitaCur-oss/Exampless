8e241e1004d3e5e8d01d75035287345df2024b2e
v <antyan2010@rambler.ru>
2023-01-31 12:13:12 +0300

    Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.

---
+++ Task_34/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+Clear();
+
+// **Задача 63:** Задайте значение N. Напишите программу,
+//  которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+string numb = GetNumber(5);
+WriteLine(numb);
+GetNumber1(10);
+
+string GetNumber(int num)
+{
+    return (num == 1)? num.ToString(): $"{GetNumber(num - 1)}, {num}";
+    // if (num == 1) return num.ToString();
+    // else return $"{GetNumber(num - 1)}, {num}";
+}
+
+void GetNumber1(int num)
+{
+    if (num == 1) Write(num);
+    else
+    {
+        GetNumber1(num - 1);
+        Write($", {num}");
+    }
+}
