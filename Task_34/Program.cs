c70f4d6f3b599152deb1d811475c8451c6be38bb
v <antyan2010@rambler.ru>
2023-02-01 13:23:24 +0300

    Help friend with task 66

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -7,12 +7,12 @@ Clear();
 // N = 6 -> "1, 2, 3, 4, 5, 6"
 
 string numb = GetNumber(5);
-WriteLine(numb);
-GetNumber1(10);
+// WriteLine(numb);
+// GetNumber1(10);
 
 string GetNumber(int num)
 {
-    return (num == 1)? num.ToString(): $"{GetNumber(num - 1)}, {num}";
+    return (num == 1) ? num.ToString() : $"{GetNumber(num - 1)}, {num}";
     // if (num == 1) return num.ToString();
     // else return $"{GetNumber(num - 1)}, {num}";
 }
@@ -25,4 +25,30 @@ void GetNumber1(int num)
         GetNumber1(num - 1);
         Write($", {num}");
     }
+}
+
+WriteLine(NumbersSumRec(15, 1));
+// int sumRecUP(int m, int n) 
+// { 
+//     if(m== n+1) return 0; 
+//     else return m + sumRecUP(m+1, n); 
+// } 
+
+int NumbersSumRec(int m, int n)
+{
+    if (m > n)
+    {
+        if (m == n - 1) return 0;
+        return m + NumbersSumRec(m - 1, n);
+        // return sumRec(m, n); 
+    }
+
+    if (m < n)
+    {
+        if (m == n + 1) return 0;
+        else return m + NumbersSumRec(m + 1, n);
+        // return sumRecUP(m, n); 
+    }
+
+    else return m;
 }
